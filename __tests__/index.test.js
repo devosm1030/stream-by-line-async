a735b2cd38000de74fef5a33307db37372bf96e2
Mike DeVos <devosm@yahoo.com>
2021-12-31 09:49:36 -0600

    initial procStream and test - wip

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,4 @@
+const { PassThrough } = require('stream')
 const { resolve: pathResolve } = require('path')
 const projectRoot = pathResolve(__dirname, '..')
 
@@ -21,5 +22,21 @@ describe('index.js unit tests', () => {
     const { StreamByLine, preserveEol, noEol } = require(pathResolve(projectRoot, 'index.js'))
     expect(new StreamByLine({ eol: preserveEol }).eol === preserveEol).toBe(true)
     expect(new StreamByLine().eol === noEol).toBe(true)
+  })
+
+  it('streams', async () => {
+    expect.assertions(3)
+    const { StreamByLine } = require(pathResolve(projectRoot, 'index.js'))
+    const mockedStream = new PassThrough()
+    const stream = new StreamByLine(mockedStream)
+    let lines = []
+    const streamDonePromise = stream.eachLine(chunk => { lines = lines.concat(chunk.split('\n')) })
+    mockedStream.push(Buffer.from('test line 1\n', 'utf8'))
+    mockedStream.push(Buffer.from('test line 2\n', 'utf8'))
+    mockedStream.end()
+    mockedStream.destroy()
+    await expect(streamDonePromise).resolves.toBe(undefined)
+    expect(lines[0]).toEqual('test line 1')
+    expect(lines[1]).toEqual('test line 2')
   })
 })
--- index.js
+++ index.js
@@ -8,28 +8,38 @@ class StreamByLine {
     this.init(...args)
   }
 
-  init (options = {}) {
+  init (stream, options = {}) {
+    this.stream = stream
     const dflts = {
       eol: noEol
     }
     Object.keys(dflts).forEach(key => {
       this[key] = options[key] !== undefined ? options[key] : dflts[key]
     })
-    this.streamDonePromise = new Promise((resolve, reject) => {
-      this.streamDone = resolve
-      this.streamError = reject
+  }
+
+  queueChunk (data) {
+    this.lineCallback(data)
+    return Promise.resolve()
+  }
+
+  async procStream () {
+    let endOfStreamResolve = null
+    const endOfStream = new Promise(resolve => { endOfStreamResolve = resolve })
+    this.stream.on('readable', async () => {
+      let chunk = null
+      while ((chunk = this.stream.read()) !== null) {
+        await this.queueChunk(chunk.toString())
+      }
     })
+    this.stream.on('end', () => { endOfStreamResolve() })
+    await endOfStream
+    // TODO - await queue processing
   }
 
-  startStream () {
-    // foreach line => this.lineCallback(line)
-    this.streamDone()
-  }
-
-  eachLine (cb) {
+  async eachLine (cb) {
     this.lineCallback = cb
-    this.startStream()
-    return this.streamDonePromise
+    await this.procStream()
   }
 }
 
